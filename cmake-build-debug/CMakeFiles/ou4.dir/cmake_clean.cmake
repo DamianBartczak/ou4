4ddfbf65955208e149b002cbaa9ea9e12d3c2120
Damian <d071213@gmail.com>
2021-02-03 16:12:16 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ou4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ou4.dir/main.cpp.obj"
+  "ou4.exe"
+  "ou4.exe.manifest"
+  "ou4.lib"
+  "ou4.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ou4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
